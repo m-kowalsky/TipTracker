51b14db858821332f5a569877856c4cb3b67855e
carlhavoc <mkowalsky87@gmail.com>
2024-11-17 14:32:09 -0700

    testing formatting for saving files to folder and writing text to files

--- testforinput.py
+++ testforinput.py
@@ -1,2 +1,23 @@
+import os
+from datetime import datetime
+
+now = datetime.now()
+today = now.strftime('%Y/%m/%d')
+month = now.strftime('%B')
+
+
+folder_path = "/Users/michaelkowalsky/Desktop/Test folder for PP1"
+
+# filename = input("What is the name of the file? ")
+complete_path = os.path.join(folder_path, month + "_tips" + ".txt")
+# complete_path = os.path.join(folder_path, filename + ".txt")
 name = input("What is your name? ")
-print(f"It's nice to meet you {name}!\n")
+
+
+file1 = open(complete_path, "a")
+
+file1.write(today + "\n\n\n")
+file1.write(name + "\n\n\n")
+file1.close()
+
+
