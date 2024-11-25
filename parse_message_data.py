79e40cb52e3205d16f4b12c997e4083e2ba05c70
carlhavoc <mkowalsky87@gmail.com>
2024-11-25 16:46:03 -0700

    linter changes

--- parse_message_data.py
+++ parse_message_data.py
@@ -1,6 +1,8 @@
-from gmail_api import get_date, get_message_snippet
 import re
- 
+
+from gmail_api import get_date, get_message_snippet
+
+
 def make_message_dict(service, message_ids):
     message_dict = {}
     for id in message_ids:
@@ -9,8 +11,8 @@ def make_message_dict(service, message_ids):
         message_dict[id] = {"date": date, "snippet": snippet}
     return message_dict
 
-def parse_snippet(snippet):
-    hours = re.search('@(.*)@', snippet)
-    tips = re.search('#(.*)#', snippet)
-    return hours, tips
 
+def parse_snippet(snippet):
+    hours = re.search("@(.*)@", snippet)
+    tips = re.search("#(.*)#", snippet)
+    return hours, tips
