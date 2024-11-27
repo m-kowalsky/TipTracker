cb3fafd64b38bcece4c5ff72d3101f403d1e88f7
carlhavoc <mkowalsky87@gmail.com>
2024-11-27 12:15:10 -0700

    finished functions for parsing snippet and creating dict for message data

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from gmail_api import (
-    # get_message_snippet,
     get_unread_tips_emails_ids,
     gmail_authenticate,
     # mark_as_read,
@@ -8,18 +7,12 @@ from parse_message_data import make_message_dict
 
 
 def main():
-    
 
     service = gmail_authenticate()
     message_ids = get_unread_tips_emails_ids(service, "subject: tips label:unread")
     message_data = make_message_dict(service, message_ids)
     print(message_data)
-    for key, value in message_data.items():
-        print(f"key: {key}")
-        print(f"value: {value}")
-        print(f"date: {value["date"]}")
-        
-        
+
     # mark_as_read(service, message_ids)
 
 
--- parse_message_data.py
+++ parse_message_data.py
@@ -1,6 +1,16 @@
-import re
-
 from gmail_api import get_date, get_message_snippet
+
+"""Takes gmail client service and list of message ids and formats it into a nested dictionary
+    {"id":
+        {
+        "date": date,
+        "snippet": {
+            "Hours": hours,
+            "Tips": tips
+            },
+        }
+    }
+"""
 
 
 def make_message_dict(service, message_ids):
@@ -8,11 +18,17 @@ def make_message_dict(service, message_ids):
     for id in message_ids:
         date = get_date(service, id)
         snippet = get_message_snippet(service, id)
-        message_dict[id] = {"date": date, "snippet": snippet}
+        message_dict[id] = {"date": date, "snippet": snippet_to_dict(snippet)}
     return message_dict
 
 
-def parse_snippet(snippet):
-    hours = re.search("@(.*)@", snippet)
-    tips = re.search("#(.*)#", snippet)
-    return hours, tips
+# Converts snippet string to json and return error if string is formatted incorrectly
+def snippet_to_dict(snippet):
+    snippet = snippet.split()
+    data = {}
+    for i in range(len(snippet)):
+        if snippet[i] == "Hours":
+            data[snippet[i].lower()] = float(snippet[i + 1])
+        if snippet[i] == "Tips":
+            data[snippet[i].lower()] = float(snippet[i + 1])
+    return data
